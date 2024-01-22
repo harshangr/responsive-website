a26abcb6033e4cb42d2653f05f785be44dda649b
harshang <ravalharshang8866@gmail.com>
2024-01-22 18:43:38 +0530

    https://github.com/harshangr/responsive-website.git

---
+++ index.js
@@ -0,0 +1,9 @@
+function toggleMenu(){
+    var nav = document.querySelector(".menu");
+        if (nav.style.display == 'none') {        
+            nav.style.display = 'block';
+        }
+        else{
+            nav.style.display = 'none';
+        }
+    }
